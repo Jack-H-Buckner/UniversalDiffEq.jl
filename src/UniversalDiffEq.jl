7a588857bc1520ff0943168aa26ce0623fb47e04
Nathan Fitzpatrick <19fitzna@gmail.com>
2024-03-27 10:44:05 -1000

    Add Depreciated Function Wrappers
    Added wrappers to handle depreciated spellings of functions.
    
    Added both depreciated and current spellings of all functions into UniversalDiffEq.jl

--- src/UniversalDiffEq.jl
+++ src/UniversalDiffEq.jl
@@ -17,6 +17,6 @@ include("MultiModelTests.jl")
 include("Optimizers.jl")
 include("Depricated.jl")
 
-export UDE, CustomDerivatives, CustomDiffernce, NNDE, DiscreteUDE, NODE, gradient_decent!, BFGS!, plot_state_estimates, plot_predictions, plot_forecast, leave_future_out_cv, LokaVolterra,plot_leave_future_out_cv, func, LorenzLokaVolterra, plot_covariates, vectorfield2d, nullclines2d, vectorfield_and_nullclines, get_right_hand_side
+export UDE, CustomDerivatives, CustomDiffernce, NNDE, DiscreteUDE, NODE, gradient_decent!, BFGS!, plot_state_estiamtes, plot_predictions, plot_forecast, leave_future_out_cv, LokaVolterra,plot_leave_future_out_cv, func, LorenzLokaVolterra, plot_covariates, vectorfield2d, nullclines2d, vectorfield_and_nullclines, get_right_hand_side, CustomDifference, plot_state_estimates, LotkaVolterra, LorenzLotkaVolterra
 
 end
