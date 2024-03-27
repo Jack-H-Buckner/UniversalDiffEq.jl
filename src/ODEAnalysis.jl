2b45d905bbdd99b9ffe22d6129a30c46a2b3bfe3
Jack Buckner <Jhbuckner@ucdavis.edu>
2024-03-27 16:49:14 -0400

    fixing issues with ODE analysis vector fields

--- src/ODEAnalysis.jl
+++ src/ODEAnalysis.jl
@@ -86,7 +86,7 @@ function nullclines2d(UDE; t = 0, upper = [0.0,0.0], lower = [1.0,1.0])
         return val 
     end 
 
-    return nullclineU1, nullclineU2, nullclineU12, nullclineU12
+    return nullclineU1, nullclineU2, nullclineU12, nullclineU21
 end 
 
 function nullclines2d(UDE,X; t = 0, upper = [0.0,0.0], lower = [1.0,1.0])
@@ -137,14 +137,16 @@ function nullclines2d(UDE,X; t = 0, upper = [0.0,0.0], lower = [1.0,1.0])
         return val 
     end 
 
-    return nullclineU1, nullclineU2, nullclineU12, nullclineU12
+    return nullclineU1, nullclineU2, nullclineU12, nullclineU21
 end 
 
 
 function vectorfield_and_nullclines(UDE;t = 0, n = 15, lower = [0.0,0.0], upper = [1.0,1.0], arrowlength = 0.2, arrow_color = "grey", xlabel = "u1", ylabel = "u2", title = "Vector Field", color_u1 = "red", color_u2 = "black",legend = :outerright)
     N1,N2,N12,N21 = UniversalDiffEq.nullclines2d(UDE,lower = lower,upper = upper,t = t)
-    x2 = -0.05:0.05:4.0; n21 = N1.(x2); n22 = N12.(x2)
-    x1 = -0.0025:0.0025:0.3; n12 = N2.(x1); n11 = N21.(x1)
+    dx2 = (upper[2] .- lower[2]) / (5*n)
+    dx1 = (upper[1] .- lower[1]) / (5*n)
+    x2 = lower[2]:dx2:upper[2]; n21 = N1.(x2); n22 = N12.(x2)
+    x1 = lower[1]:dx1:upper[1]; n12 = N2.(x1); n11 = N21.(x1)
 
     p1 = UniversalDiffEq.vectorfield2d(UDE, t = t, title = title , n = n, lower = lower , upper =  upper , arrowlength=arrowlength,arrow_color = arrow_color)
     Plots.plot!(p1,n21,x2,  label = L"\frac{dx}{dt} = 0", color= color_u1 ,width = 2,xlabel=xlabel,ylabel=ylabel)
