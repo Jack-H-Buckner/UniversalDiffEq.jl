019cd5c0ad3028ac9da031aa314dad53e8151bc3
Jorge Arroyo-Esquivel <46660213+jarroyoe@users.noreply.github.com>
2024-06-10 13:40:34 -0700

    Added EasyModels frontend

---
+++ UniversalDiffEq.jl
@@ -0,0 +1,25 @@
+module UniversalDiffEq
+
+using DiffEqFlux, DifferentialEquations, Optimization, OptimizationOptimisers, OptimizationOptimJL, ComponentArrays, Lux, Random, StatsBase, DelimitedFiles, Plots, DataFrames, Distributions, Roots, LaTeXStrings, NLsolve, FiniteDiff, LinearAlgebra
+
+include("covariates.jl")
+include("SimulateData.jl")
+include("helpers.jl")
+include("ObservationModels.jl")
+include("ProcessModels.jl")
+include("LossFunctions.jl")
+include("Regularization.jl")
+include("Models.jl")
+include("MultipleTimeSeries.jl")
+include("ModelTesting.jl")
+include("ODEAnalysis.jl")
+include("MultiModelTests.jl")
+include("Optimizers.jl")
+include("CrossValidation.jl")
+include("Deprecated.jl")
+include("EasyModels.jl")
+
+
+export UDE, CustomDerivatives, CustomDiffernce, NNDE, DiscreteUDE, NODE, gradient_decent!, BFGS!, plot_state_estiamtes, plot_predictions, plot_forecast, leave_future_out_cv, LokaVolterra,plot_leave_future_out_cv, func, LorenzLokaVolterra, plot_covariates, vectorfield2d, nullclines2d, vectorfield_and_nullclines, get_right_hand_side, CustomDifference, plot_state_estimates, LotkaVolterra, LorenzLotkaVolterra, kfold_cv, gradient_descent!, equilibrium_and_stability, EasyNODE, EasyUDE
+
+end
