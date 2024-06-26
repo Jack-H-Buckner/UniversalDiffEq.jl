26a0226714a20d2ccd6a35f349f369f44640b66e
Jorge Arroyo Esquivel <jorgearroyo0731@gmail.com>
2024-06-26 08:36:45 -0700

    Expanded BayesNODEtests.jl

--- test/BayesNODEtests.jl
+++ test/BayesNODEtests.jl
@@ -7,3 +7,9 @@ training_data = data[1:(end-6),:]
 model = BayesianNODE(training_data)
 NUTS!(model,samples = 10)
 SGLD!(model,samples = 10)
+
+
+plot_predictions(model)
+test_data = data[(end-5):end,:]
+plt,(p1,p2) = plot_forecast(model, test_data)
+plot_predictions(model, test_data)
