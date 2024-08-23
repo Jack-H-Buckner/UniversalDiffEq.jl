e5b1b37316b1263836192d505c69bb11c6298aa0
Jorge Arroyo Esquivel <jorgearroyo0731@gmail.com>
2024-08-23 14:49:48 -0700

    Added EasyNODE tests

---
+++ test/EasyNODEtests.jl
@@ -0,0 +1,14 @@
+using UniversalDiffEq, DataFrames
+
+data,plt = LotkaVolterra()
+training_data = data[1:(end-6),:]
+
+# test Bayesian NODEs
+model = EasyNODE(training_data)
+
+function known_dynamics!(du,u,a,t)
+    du .= a*u .+ b #some function here
+    return du
+end
+initial_parameters = (a = 1, b = 0.1)
+easy_model = EasyUDE(training_data,known_dynamics!,initial_parameters)
