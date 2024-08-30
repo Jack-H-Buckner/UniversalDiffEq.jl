21afcd91ec595453157d84ae70a273a254e532fc
Jack Buckner <Jhbuckner@ucdavis.edu>
2024-08-30 16:50:29 -0400

    added data files and plots for Jonada Range data tutorial

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,7 @@ using UniversalDiffEq
 using Test
 
 @testset "UniversalDiffEq.jl" begin
+    include("UDEtests.jl")
     include("NODEtests.jl")
     include("BayesNODEtests.jl")
     include("EasyNODEtests.jl")
