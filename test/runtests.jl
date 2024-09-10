044a23f2243b8b7e172237ee2ca3e14f72aa125f
Jack Buckner <Jhbuckner@ucdavis.edu>
2024-09-10 15:06:10 -0400

    fixed that directory issue in tests?

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using UniversalDiffEq
 using Test
 
 @testset "UniversalDiffEq.jl" begin 
-    include("../test/UDEtests.jl")
+    include("UDEtests.jl")
     include("NODEtests.jl")
     include("BayesNODEtests.jl")
     include("EasyNODEtests.jl")
