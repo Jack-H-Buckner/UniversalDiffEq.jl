f9afd1a109f00a53321e3453912a2ff36030cdaa
Zechariah Meunier <72064608+zechmeunier@users.noreply.github.com>
2024-12-18 12:39:43 -0800

    Set random seed for reproducibility, fix typos

--- src/NeuralNetworkConstructors.jl
+++ src/NeuralNetworkConstructors.jl
@@ -9,16 +9,17 @@ end
 """
     SimpleNeuralNetwork(inputs,outputs; kwargs ...)
 
-Builds a neural network object and returns randomly initialized parameter values. The neural network object can be evaluated like a function it takes two arguments: a vector  `x` and named tuple with the neural network weights and biases `parameters`.  
+Builds a neural network object and returns randomly initialized parameter values. The neural network object can be evaluated as a function that takes two arguments: a vector `x` and a named tuple with the neural network weights and biases `parameters`.  
 
     # kwargs 
 
-- `hidden`: the number of neurons in the hidden layer. The default is 10
-- `nonlinearity`: the activation funciton used in the neurla network. The default is the hyperbolic tangent function `tanh`.
-- `seed` : random number generator seed for initializing the neural network weights
+- `hidden`: the number of neurons in the hidden layer. The default is 10.
+- `nonlinearity`: the activation function used in the neural network. The default is the hyperbolic tangent function `tanh`.
+- `seed`: random number generator seed for initializing the neural network weights.
 """
 function SimpleNeuralNetwork(inputs,outputs; hidden = 10, nonlinearity = tanh, seed = 123)
     NN = Lux.Chain(Lux.Dense(inputs,hidden,nonlinearity),Lux.Dense(hidden,outputs))
+    Random.seed!(seed)
     rng = Random.default_rng() 
     parameters, states = Lux.setup(rng,NN) 
     return SimpleNeuralNetwork(inputs,outputs,hidden,NN,states), parameters
@@ -47,17 +48,18 @@ end
 """
     MultiTimeSeriesNetwork(inputs,series,outputs; kwargs...)
 
-Builds a neural network object and returns randomly initialized parameter values. The neural network object can be evaluated like a function it takes three  arguments: a vector  `x`, an integer `i` and named tuple with the neural network weights and biases `parameters`.  
+Builds a neural network object and returns randomly initialized parameter values. The neural network object can be evaluated as a function that takes three arguments: a vector `x`, an integer `i`, and a named tuple with the neural network weights and biases `parameters`.  
 
     # kwargs 
 
-- `hidden`: the number of neurons in the hidden layer. The default is 10
-- `nonlinearity`: the activation funciton used in the neurla network. The default is the hyperbolic tangent function `tanh`.
-- `seed` : random number generator seed for initializing the neural network weights
-- `distance` : deterines the level of differnce between the functions estimated for each time series. the defualt is 1.0.
+- `hidden`: the number of neurons in the hidden layer. The default is 10.
+- `nonlinearity`: the activation function used in the neural network. The default is the hyperbolic tangent function `tanh`.
+- `seed` : random number generator seed for initializing the neural network weights.
+- `distance` : determines the level of difference between the functions estimated for each time series. The default is 1.0.
 """
 function MultiTimeSeriesNetwork(inputs,series,outputs; hidden = 10, nonlinearity = tanh, seed = 123, distance = 1.0)
     NN = Lux.Chain(Lux.Dense(inputs+series,hidden,nonlinearity),Lux.Dense(hidden,outputs))
+    Random.seed!(seed)
     rng = Random.default_rng() 
     parameters, states = Lux.setup(rng,NN) 
     one_hot = zeros(series)
@@ -77,6 +79,4 @@ function (model::MultiTimeSeriesNetwork)(x,i,parameters)
     out = model.NN(inputs,parameters,model.states)[1]
     model.one_hot[round(Int,i)] = 0
     return out
-end 
-
-
+end
