d322e54eb8ee92fcb53418de65c66e8646c8fca5
Jack Buckner <Jhbuckner@ucdavis.edu>
2025-01-22 12:04:34 -0500

    fixing issues in tests atempt 5

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using Test
 
 @testset "UniversalDiffEq.jl" begin 
     include("UDETests.jl")
-    include("NODEs.jl")
+    include("NODEtests.jl")
     include("MultiUDE.jl")
     # include("BayesNODEtests.jl")
     # include("EasyNODEtests.jl")
