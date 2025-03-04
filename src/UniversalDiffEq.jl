d6273f630211aad2f6ef2aa633afb23240c6e498
Jorge Arroyo Esquivel <jorgearroyo0731@gmail.com>
2025-03-04 11:02:13 -0800

    Added load/save functions

--- src/UniversalDiffEq.jl
+++ src/UniversalDiffEq.jl
@@ -1,6 +1,6 @@
 module UniversalDiffEq
 
-using DiffEqFlux, OrdinaryDiffEq, StochasticDiffEq, Optimization, OptimizationOptimisers, OptimizationOptimJL, ComponentArrays, Lux, Random, StatsBase, DelimitedFiles, Plots, DataFrames, Distributions, Roots, LaTeXStrings, NLsolve, FiniteDiff, LinearAlgebra, AdvancedHMC, MCMCChains, Interpolations, Optim, CategoricalArrays, Zygote, RegularizationTools, DataInterpolations
+using DiffEqFlux, OrdinaryDiffEq, StochasticDiffEq, Optimization, OptimizationOptimisers, OptimizationOptimJL, ComponentArrays, Lux, Random, StatsBase, DelimitedFiles, Plots, DataFrames, Distributions, Roots, LaTeXStrings, NLsolve, FiniteDiff, LinearAlgebra, AdvancedHMC, MCMCChains, Interpolations, Optim, CategoricalArrays, Zygote, RegularizationTools, DataInterpolations, JSON
 
 
 include("covariates.jl")
@@ -23,6 +23,6 @@ include("NeuralNetworkConstructors.jl")
 include("UnscentedKalmanFilter.jl")
 include("LossFunctionConstructors.jl")
 
-export UDE, MultiUDE, CustomDerivatives, CustomModel, NNDE, NODE, MultiNODE, MultiCustomDerivatives ,train!, plot_predictions, plot_forecast, leave_future_out_cv, vectorfield_and_nullclines, get_right_hand_side, CustomDifference, plot_state_estimates, LotkaVolterra, LorenzLotkaVolterra, kfold_cv, gradient_descent!, equilibrium_and_stability, EasyNODE, EasyUDE, BayesianNODE, BayesianUDE, NUTS!, SGLD!, predict, get_parameters, plot_bifurcation_diagram, bifurcation_data, cross_validation_kfold, simulate_coral_data, SimpleNeuralNetwork
+export UDE, MultiUDE, CustomDerivatives, CustomModel, NNDE, NODE, MultiNODE, MultiCustomDerivatives ,train!, plot_predictions, plot_forecast, leave_future_out_cv, vectorfield_and_nullclines, get_right_hand_side, CustomDifference, plot_state_estimates, LotkaVolterra, LorenzLotkaVolterra, kfold_cv, gradient_descent!, equilibrium_and_stability, EasyNODE, EasyUDE, BayesianNODE, BayesianUDE, NUTS!, SGLD!, predict, get_parameters, plot_bifurcation_diagram, bifurcation_data, cross_validation_kfold, simulate_coral_data, SimpleNeuralNetwork, save_model, load_model
 
 end
