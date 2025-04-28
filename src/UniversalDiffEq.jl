bbd99b9433fcf72a3348a8569cd3d41b6a4f0ff1
Jack Buckner <Jhbuckner@ucdavis.edu>
2025-04-28 10:13:27 -0400

    removed all plotting function and Bayesian models along with their dependencies

--- src/UniversalDiffEq.jl
+++ src/UniversalDiffEq.jl
@@ -1,10 +1,9 @@
 module UniversalDiffEq
 
-using DiffEqFlux, OrdinaryDiffEq, StochasticDiffEq, Optimization, OptimizationOptimisers, OptimizationOptimJL, ComponentArrays, Lux, Random, StatsBase, DelimitedFiles, Plots, DataFrames, Distributions, Roots, LaTeXStrings, NLsolve, FiniteDiff, LinearAlgebra, AdvancedHMC, MCMCChains, Interpolations, Optim, CategoricalArrays, Zygote, RegularizationTools, DataInterpolations, JSON, CSV
+using DiffEqFlux, OrdinaryDiffEq, Optimization, OptimizationOptimisers, OptimizationOptimJL, ComponentArrays, Lux, Random, StatsBase,  DataFrames,  LinearAlgebra, Interpolations, Optim, CategoricalArrays, Zygote, RegularizationTools, DataInterpolations, CSV
 
 
 include("covariates.jl")
-include("SimulateData.jl")
 include("ObservationModels.jl")
 include("ProcessModels.jl")
 include("LossFunctions.jl")
@@ -13,7 +12,6 @@ include("Models.jl")
 include("MultiProcessModel.jl")
 include("MultipleTimeSeries.jl")
 include("ModelTesting.jl")
-include("ODEAnalysis.jl")
 include("MultiModelTests.jl")
 include("helpers.jl")
 include("Optimizers.jl")
@@ -23,6 +21,6 @@ include("UnscentedKalmanFilter.jl")
 include("LossFunctionConstructors.jl")
 
 
-export UDE, MultiUDE, CustomDerivatives, CustomModel, NNDE, NODE, MultiNODE, MultiCustomDerivatives ,train!, plot_predictions, plot_forecast, leave_future_out, vectorfield_and_nullclines, get_right_hand_side, CustomDifference, plot_state_estimates, LotkaVolterra, LorenzLotkaVolterra, gradient_descent!, equilibrium_and_stability, EasyNODE, EasyUDE, BayesianNODE, BayesianUDE, NUTS!, SGLD!, predict, get_parameters, plot_bifurcation_diagram, bifurcation_data, leave_future_out, simulate_coral_data, SimpleNeuralNetwork, save_model, load_model
+export UDE, MultiUDE, CustomDerivatives, CustomDifference, MultiCustomDerivatives , MultiCustomDifference, train!, leave_future_out, get_right_hand_side, predictions, SimpleNeuralNetwork
 
 end
